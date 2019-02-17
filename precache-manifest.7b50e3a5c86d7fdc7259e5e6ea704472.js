2c3699ea76766b936f87be37f0aa8a1ba6218e52
Zheng Qi <446488348@qq.com>
2019-02-17 19:04:28 +0000

    Updates

---
+++ precache-manifest.7b50e3a5c86d7fdc7259e5e6ea704472.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9e0b58d846368f426a94",
+    "url": "/robofriends-redux/static/js/runtime~main.9e0b58d8.js"
+  },
+  {
+    "revision": "78d58d7669eb258133b2",
+    "url": "/robofriends-redux/static/js/main.78d58d76.chunk.js"
+  },
+  {
+    "revision": "68aa03a41d5b6b2fbdf5",
+    "url": "/robofriends-redux/static/js/1.68aa03a4.chunk.js"
+  },
+  {
+    "revision": "78d58d7669eb258133b2",
+    "url": "/robofriends-redux/static/css/main.9a55813b.chunk.css"
+  },
+  {
+    "revision": "68aa03a41d5b6b2fbdf5",
+    "url": "/robofriends-redux/static/css/1.bb21eeb2.chunk.css"
+  },
+  {
+    "revision": "0acc817f0ff488885621e45d0b799f0c",
+    "url": "/robofriends-redux/index.html"
+  }
+];
